3d27288150b5354fcef52a7d0d918a3f1eb9d89f
Drew Spooner <drewspooner@tamu.edu>
2021-04-20 14:54:32 -0500

    Added seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: 'Yes', author:'Confirmation', genre: 'Green')
+Book.create(title: 'No', author:'Decline', genre: 'Red')
+Book.create(title: 'What?', author:'Question', genre: 'Blue')
+Book.create(title: 'I need help', author:'Help', genre: 'Yellow')
